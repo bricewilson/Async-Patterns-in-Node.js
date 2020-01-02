9ec10d5f33a8b40e700927fd610a8f2daba7cf4d
Brice Wilson <brice@5wilsons.net>
2020-01-02 17:06:20 -0500

    config changes

--- server/routes/clothing.js
+++ server/routes/clothing.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const datafile = 'server/data/clothing.json';
 const router = express.Router();
 
-/* GET all clothing and POST new clothing */
+/* GET all clothing */
 router.route('/')
   .get(function(req, res) {
 
@@ -12,45 +12,6 @@ router.route('/')
 
     res.send(clothingData);
 
-  })
-
-  .post(function(req, res) {
-
-    let data = getClothingData();
-    let nextID = getNextAvailableID(data);
-
-    let newClothing = {
-      clothingID: nextID,
-      itemName: req.body.itemName,
-      price: req.body.price
-    };
-
-    data.push(newClothing);
-
-    saveClothing(data);
-
-    res.set('Content-Type', 'application/json');
-    res.status(201).send(newClothing);
   });
-
-function saveClothing(data) {
-  fs.writeFile(datafile, JSON.stringify(data, null, 4), function (err) {
-    if (err) {
-      console.log(err);
-    }
-  });
-}
-
-function getNextAvailableID(allClothing) {
-
-  let maxID = 0;
-
-  allClothing.forEach(function(element, index, array) {
-    if(element.clothingID > maxID) {
-      maxID = element.clothingID;
-    }
-  });
-  return ++maxID;
-}
 
 module.exports = router;
