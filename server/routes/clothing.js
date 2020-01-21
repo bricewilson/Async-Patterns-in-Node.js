34e3636771d1272845732cb820a18568cb1b00fd
Brice Wilson <brice@5wilsons.net>
2020-01-21 11:33:46 -0500

    recorded demo 1

--- server/routes/clothing.js
+++ server/routes/clothing.js
@@ -7,29 +7,31 @@ const router = express.Router();
 router.route('/')
   .get(function(req, res) {
 
-    getClothingData((err, data) => {
-      if (err) {
-        console.log(err);
-      }
-      else {
-        console.log('Returning clothing data');
+    getClothingData()
+      .then(data => {
+        console.log('Returning clothing data to browser.');
         res.send(data);
-      }
-    });
+      })
+      .catch(error => res.status(500).send(error))
+      .finally(() => console.log('All done processing promise.'));
 
     console.log('Doing more work');
   });
 
-function getClothingData(callback) {
-  fs.readFile(datafile, 'utf8', (err, data) => {
-    if (err) {
-      callback(err, null);
-    }
-    else {
-      let clothingData = JSON.parse(data);
-      callback(null, clothingData);
-    }
+function getClothingData() {
+
+  return new Promise((resolve, reject) => {
+    fs.readFile(datafile, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      }
+      else {
+        let clothingData = JSON.parse(data);
+        resolve(clothingData);
+      }
+    });
   });
+
 }  
 
 module.exports = router;
