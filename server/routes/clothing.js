7d9cf782569b792334363e3c3f0e3f5c9c79945d
Brice Wilson <brice@5wilsons.net>
2020-01-22 12:35:15 -0500

    recorded demo 3

--- server/routes/clothing.js
+++ server/routes/clothing.js
@@ -1,36 +1,52 @@
 const express = require('express');
 const fs = require('fs');
+const fsPromises = require('fs').promises;
 const datafile = 'server/data/clothing.json';
 const router = express.Router();
 
 /* GET all clothing */
 router.route('/')
-  .get(function(req, res) {
+  .get(async function(req, res) {
 
-    getClothingData()
-      .then(data => {
-        console.log('Returning clothing data to browser.');
-        res.send(data);
-      })
-      .catch(error => res.status(500).send(error))
-      .finally(() => console.log('All done processing promise.'));
+    try {
+      let data = await getClothingData();
+      console.log('Returning async data.');
+      res.send(data);
+    }
+    catch (error) {
+      res.status(500).send(error);
+    }
 
-    console.log('Doing more work');
+    // getClothingData()
+    //   .then(data => {
+    //     console.log('Returning clothing data to browser.');
+    //     res.send(data);
+    //   })
+    //   .catch(error => res.status(500).send(error))
+    //   .finally(() => console.log('All done processing promise.'));
+
   });
 
-function getClothingData() {
+async function getClothingData() {
 
-  return new Promise((resolve, reject) => {
-    fs.readFile(datafile, 'utf8', (err, data) => {
-      if (err) {
-        reject(err);
-      }
-      else {
-        let clothingData = JSON.parse(data);
-        resolve(clothingData);
-      }
-    });
-  });
+  let rawData = await fsPromises.readFile(datafile, 'utf8');
+  let clothingData = JSON.parse(rawData);
+
+  console.log(clothingData);
+
+  return clothingData;
+
+  // return new Promise((resolve, reject) => {
+  //   fs.readFile(datafile, 'utf8', (err, data) => {
+  //     if (err) {
+  //       reject(err);
+  //     }
+  //     else {
+  //       let clothingData = JSON.parse(data);
+  //       resolve(clothingData);
+  //     }
+  //   });
+  // });
 
 }  
 
