0a3b20629b9612fef022bd1e1272c27c862a596d
Brice Wilson <brice@5wilsons.net>
2020-02-13 17:35:11 -0500

    recorded demo 2

--- threads.js
+++ threads.js
@@ -1,20 +1,32 @@
-const { Worker, isMainThread, parentPort } = require('worker_threads');
+const { Worker, isMainThread, parentPort, workerData } = require('worker_threads');
 
 if(isMainThread) {
   console.log('Starting the main thread.');
 
-  const worker = new Worker(__filename);
+  const worker = new Worker(__filename, {
+    workerData: {
+      outputPrefix: 'Received message',
+      timeToWaste: 500
+    }
+  });
 
   worker.on('message', (msg) => {
     console.log(`Worker: ${msg}`);
   });
 
+  worker.postMessage('Done with my work.');
+
   console.log('Still in the main thread.');
 } else {
+
+  parentPort.on('message', (msg) => {
+    console.log(`${workerData.outputPrefix}: ${msg}`);
+  });
+
   parentPort.postMessage('Getting started');
-  wasteTime(2000);
+  wasteTime(workerData.timeToWaste);
   parentPort.postMessage('In the middle');
-  wasteTime(2000);
+  wasteTime(workerData.timeToWaste);
   parentPort.postMessage('All done');
 }
 
